d38842ae229af9e6d028b7ca4b6c600efefa7352
Stefanie Davis <davisstefanie2010@gmail.com>
2019-09-17 23:14:41 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -10,7 +10,7 @@ def fourth_challenge
           :name => "Benvolio",
           :age => "17",
           :attitude => "worried"
-         }
+         },
          {
           :name => "Mercutio",
           :age => "18",
@@ -28,7 +28,7 @@ def fourth_challenge
           :name => "Steven",
           :age => "30",
           :attitude => "confused"
-         }
+         },
          {
           :name => "Nurse",
           :age => "44",
